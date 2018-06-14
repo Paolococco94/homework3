18d75f9433684402f7e7714865e75faa885ba234
Paolococco94 <37830248+Paolococco94@users.noreply.github.com>
2018-06-14 10:16:40 +0200

    Add files via upload

---
+++ build/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake
@@ -0,0 +1,74 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/paolo/catkin_ws/src/git/ORB_SLAM2/src/Converter.cc" "/home/paolo/catkin_ws/src/git/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Converter.cc.o"
+  "/home/paolo/catkin_ws/src/git/ORB_SLAM2/src/Frame.cc" "/home/paolo/catkin_ws/src/git/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Frame.cc.o"
+  "/home/paolo/catkin_ws/src/git/ORB_SLAM2/src/FrameDrawer.cc" "/home/paolo/catkin_ws/src/git/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/FrameDrawer.cc.o"
+  "/home/paolo/catkin_ws/src/git/ORB_SLAM2/src/Initializer.cc" "/home/paolo/catkin_ws/src/git/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Initializer.cc.o"
+  "/home/paolo/catkin_ws/src/git/ORB_SLAM2/src/KeyFrame.cc" "/home/paolo/catkin_ws/src/git/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/KeyFrame.cc.o"
+  "/home/paolo/catkin_ws/src/git/ORB_SLAM2/src/KeyFrameDatabase.cc" "/home/paolo/catkin_ws/src/git/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/KeyFrameDatabase.cc.o"
+  "/home/paolo/catkin_ws/src/git/ORB_SLAM2/src/LocalMapping.cc" "/home/paolo/catkin_ws/src/git/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/LocalMapping.cc.o"
+  "/home/paolo/catkin_ws/src/git/ORB_SLAM2/src/LoopClosing.cc" "/home/paolo/catkin_ws/src/git/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/LoopClosing.cc.o"
+  "/home/paolo/catkin_ws/src/git/ORB_SLAM2/src/Map.cc" "/home/paolo/catkin_ws/src/git/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Map.cc.o"
+  "/home/paolo/catkin_ws/src/git/ORB_SLAM2/src/MapDrawer.cc" "/home/paolo/catkin_ws/src/git/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/MapDrawer.cc.o"
+  "/home/paolo/catkin_ws/src/git/ORB_SLAM2/src/MapPoint.cc" "/home/paolo/catkin_ws/src/git/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/MapPoint.cc.o"
+  "/home/paolo/catkin_ws/src/git/ORB_SLAM2/src/ORBextractor.cc" "/home/paolo/catkin_ws/src/git/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/ORBextractor.cc.o"
+  "/home/paolo/catkin_ws/src/git/ORB_SLAM2/src/ORBmatcher.cc" "/home/paolo/catkin_ws/src/git/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/ORBmatcher.cc.o"
+  "/home/paolo/catkin_ws/src/git/ORB_SLAM2/src/Optimizer.cc" "/home/paolo/catkin_ws/src/git/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Optimizer.cc.o"
+  "/home/paolo/catkin_ws/src/git/ORB_SLAM2/src/PnPsolver.cc" "/home/paolo/catkin_ws/src/git/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/PnPsolver.cc.o"
+  "/home/paolo/catkin_ws/src/git/ORB_SLAM2/src/Sim3Solver.cc" "/home/paolo/catkin_ws/src/git/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Sim3Solver.cc.o"
+  "/home/paolo/catkin_ws/src/git/ORB_SLAM2/src/System.cc" "/home/paolo/catkin_ws/src/git/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/System.cc.o"
+  "/home/paolo/catkin_ws/src/git/ORB_SLAM2/src/Tracking.cc" "/home/paolo/catkin_ws/src/git/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Tracking.cc.o"
+  "/home/paolo/catkin_ws/src/git/ORB_SLAM2/src/Viewer.cc" "/home/paolo/catkin_ws/src/git/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Viewer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  "qh_QHpointer"
+  "vtkFiltersFlowPaths_AUTOINIT=1(vtkFiltersParallelFlowPaths)"
+  "vtkIOExodus_AUTOINIT=1(vtkIOParallelExodus)"
+  "vtkIOGeometry_AUTOINIT=1(vtkIOMPIParallel)"
+  "vtkIOImage_AUTOINIT=1(vtkIOMPIImage)"
+  "vtkIOSQL_AUTOINIT=2(vtkIOMySQL,vtkIOPostgreSQL)"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL)"
+  "vtkRenderingCore_AUTOINIT=4(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingFreeTypeOpenGL,vtkRenderingOpenGL)"
+  "vtkRenderingFreeType_AUTOINIT=2(vtkRenderingFreeTypeFontConfig,vtkRenderingMatplotlib)"
+  "vtkRenderingLIC_AUTOINIT=1(vtkRenderingParallelLIC)"
+  "vtkRenderingVolume_AUTOINIT=1(vtkRenderingVolumeOpenGL)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/vtk-6.2"
+  "/usr/include/freetype2"
+  "/usr/include/x86_64-linux-gnu/freetype2"
+  "/usr/include/jsoncpp"
+  "/usr/include/x86_64-linux-gnu"
+  "/usr/include/python2.7"
+  "/usr/lib/openmpi/include/openmpi/opal/mca/event/libevent2021/libevent"
+  "/usr/lib/openmpi/include/openmpi/opal/mca/event/libevent2021/libevent/include"
+  "/usr/lib/openmpi/include"
+  "/usr/lib/openmpi/include/openmpi"
+  "/usr/include/hdf5/openmpi"
+  "/usr/include/libxml2"
+  "/usr/include/tcl"
+  "../"
+  "../include"
+  "/usr/local/include/eigen3"
+  "/home/paolo/catkin_ws/src/git/Pangolin/include"
+  "/home/paolo/catkin_ws/src/git/Pangolin/build/src/include"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
